e7395ba107048400b280b4e6322bccd3edeee07d
Elias Tamer <eliastamer3@gmail.com>
2024-05-30 20:21:26 +0300

    added error handeling to my api call

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.title("Weather Forecast for the next days")
 place = st.text_input("Place:")
 days = st.slider("Forecast Days", min_value=1, max_value=5, help="Select the number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
-st.subheader(f"{option} for the next {days} days in {place}")
+
 
 
 images = {"Clear" : "images/clear.png", "Clouds" : "images/cloud.png",
@@ -15,17 +15,22 @@ images = {"Clear" : "images/clear.png", "Clouds" : "images/cloud.png",
             }
 
 if place:
+    try:
         #Get the temperature/sky data
-    filtered_data = get_data(place, days)
-
-    if option == "Temperature":
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        figure = px.line(x=dates, y=temperatures , labels={"x" : "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        filtered_data = get_data(place, days)
         
+        st.subheader(f"{option} for the next {days} days in {place}")
         
-    if option == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-        filtered_paths = [images[condition] for condition in filtered_data]
-        st.image(filtered_paths, width=115)
+        if option == "Temperature":
+            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures , labels={"x" : "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+            
+            
+        if option == "Sky":
+            filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+            filtered_paths = [images[condition] for condition in filtered_data]
+            st.image(filtered_paths, width=115)
+    except:
+        st.write("Selected city doesn't exist.")
